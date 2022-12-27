62bb33e0f60ca8e72fbf1ec94d59bfbf5dc0a0d9
Kanik Kapoor <kanik@benzeenautoparts.com>
2022-12-27 17:47:40 +0530

    UI fixes

--- src/components/conrats.js
+++ src/components/conrats.js
@@ -10,17 +10,15 @@ const Congrats = () => {
                 <div className="congrats">
                     <h1>Congrat<span className="hide">ulation</span>s !</h1>
                     <div className="done">
-                       <img src={img} alt="hello"/>
+                       <img src={img} alt="hello" x="0px" y="0px" viewBox="0 0 37 37" />
                     </div>
                     <div className="text">
-                        <p>You have successfully booked an appointment with us. <br/>Here are your details<br/>Date: 12.12.12<br />
-                            Time: 11am<br/>
-                                ID: 12324
-                        </p>
-                        <p>Eagerly awaiting your visit
+                        <p>You have successfully redeemed your coupon<br/><br />You have won<br /><br /></p>
+                        <p className="discount">100 Rs</p><br/>
+                        <p>Share the screenshot with the dealer to receive your price
                         </p>
                     </div>
-                    <p className="regards">Regards, Team Tarini Netradham</p>
+                    <p className="regards">Regards, VDROP RO SYSTEM</p>
                 </div>
             </div>
 
